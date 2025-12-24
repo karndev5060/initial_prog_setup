211277954555279a99641fd608e92f0aab61db30
Karndevsinh Vala <92354114+karndev5060@users.noreply.github.com>
2025-12-25 02:31:26 +0530

    Update D1 macros

--- cp_template.cpp
+++ cp_template.cpp
@@ -2,12 +2,28 @@
 using namespace std;
 
 //  MACROS ARE LISTED HERE
+#define ll                  long long
+#define ld                  long double
 
-#define test int t; cin>>t; while (t--)
-#define f(i, n) for(int i = 0; i < n; i++)
-#define fll(i, n) for(long long i = 0; i < n; i++)
-#define p(x) cout<<x<<'\n'; //for simple printing and switching to newline
-#define SOS(x, str) int x = (int) str.size(); //to find length of string
+
+/// VECTORS
+#define vi                  vector<int>
+#define vl                  vector<ll>
+
+
+/// STRINGS
+#define SOS(x, str)         int x = (int) str.size(); 
+#define ascii(x, char)      int x = (int) char;
+
+
+//OTHERS
+#define test                int t; cin>>t; while (t--)
+#define f(i, n)             auto i = n; for(i = 0; i < n; i++)
+#define finit(i, a, n)      auto i = n; for(i = a; i < n; i++)
+#define pn(x)               cout<<x<<'\n'; 
+#define ps(x)               cout<<x<<' ';
+#define nl                  cout<<'\n';
+
 
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
 
