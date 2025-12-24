ce4c90173ecc027053d8cdf2d4a675893a21651d
Karndevsinh Vala <92354114+karndev5060@users.noreply.github.com>
2025-12-24 20:58:01 +0530

    Initial template created

---
+++ cp_template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//  MACROS ARE LISTED HERE
+
+#define test int t; cin>>t; while (t--)
+#define f(i, n) for(int i = 0; i < n; i++)
+#define fll(i, n) for(long long i = 0; i < n; i++)
+#define p(x) cout<<x<<'\n';
+
+//  CODE STARTS HERE
+
+int main()
+{
+
+}
