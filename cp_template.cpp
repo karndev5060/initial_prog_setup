42dc36e8708218ed570b332010e4262c68d92040
Karndevsinh Vala <92354114+karndev5060@users.noreply.github.com>
2025-12-24 21:23:08 +0530

    Update cp_template for finding length of string
    Added comments to macros for better understanding.

--- cp_template.cpp
+++ cp_template.cpp
@@ -6,7 +6,9 @@ using namespace std;
 #define test int t; cin>>t; while (t--)
 #define f(i, n) for(int i = 0; i < n; i++)
 #define fll(i, n) for(long long i = 0; i < n; i++)
-#define p(x) cout<<x<<'\n';
+#define p(x) cout<<x<<'\n'; //for simple printing and switching to newline
+#define SOS(x, str) int x = (int) str.size(); //to find length of string
+
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
 
 //  CODE STARTS HERE
