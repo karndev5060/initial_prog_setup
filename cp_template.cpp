500410ec38d355077b760cdac8e9cd89c102523d
Karndevsinh Vala <92354114+karndev5060@users.noreply.github.com>
2025-12-25 13:01:21 +0530

    Update Macros

--- cp_template.cpp
+++ cp_template.cpp
@@ -1,34 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 //  MACROS ARE LISTED HERE
-#define ll                  long long
-#define ld                  long double
-
-
 /// VECTORS
 #define vi                  vector<int>
-#define vl                  vector<ll>
-
-
+ 
 /// STRINGS
 #define SOS(x, str)         int x = (int) str.size(); 
 #define ascii(x, char)      int x = (int) char;
-
-
+ 
 //OTHERS
 #define test                int t; cin>>t; while (t--)
-#define f(i, n)             auto i = n; for(i = 0; i < n; i++)
-#define finit(i, a, n)      auto i = n; for(i = a; i < n; i++)
+#define f(i, n)             for(int i = 0; i < n; i++)
+#define fi(i, a, n)         for(int i = a; i < n; i++)
+#define fll(i, n)           for(long long i = 0; i < n; i++)
+#define fill(i, a, n)       for(long long i = a; i < n; i++)
+#define InArr               int n; cin>>n; int arr[n];f(i, n) cin>>arr[i];
 #define pn(x)               cout<<x<<'\n'; 
 #define ps(x)               cout<<x<<' ';
 #define nl                  cout<<'\n';
-
-
-#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
-
+ 
+#define FAST_IO             ios_base::sync_with_stdio(false); cin.tie(NULL);
+ 
 //  CODE STARTS HERE
-
 int main()
 {
     FAST_IO
